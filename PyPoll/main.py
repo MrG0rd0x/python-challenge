73aa0600fb43d9ad34259b439a002aeadc9051aa
Kas <52807697+MrG0rd0x@users.noreply.github.com>
2023-09-24 22:39:09 -0500

    Change a few things

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,73 +1,43 @@
 import os
 import csv
 
-# Set path for file
-csvpath = os.path.join("resources", "election_data.csv")
-
-#Set up variables
 rowcount = 1
-list_of_candidates = []
-dict = {}
-winner_count = 0
+votes = {}
+# Some comment here about the expected structure of the CSV file
+with open(os.path.join('resources', 'election_data.csv'), encoding='UTF-8') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    next(csv_reader)
+    # Now we're on row 2
 
-
-# Open the CSV using the UTF-8 encoding
-with open(csvpath, encoding='UTF-8') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=",")
-    next (csvreader)
-    #Now we're on row 2
-
-    for row in csvreader:
+    for row in csv_reader:
         rowcount +=1
         current_candidate = row[2]
         
-        #Add the current candidate to the list
-        #Create the dictionary
-        if str(current_candidate) not in list_of_candidates:
-            list_of_candidates.append(current_candidate)
-            dict[current_candidate] = 0
-        
-        #Adds the vote count to the dictionary key
-        dict[current_candidate] += 1
+        # Add the current candidate to the list
+        votes[current_candidate] = votes.get(current_candidate, 0) + 1
 
-#Calculate number of candidates
-num_candidates = len(dict)
-
-#Prints results to the terminal        
-print("Election Results")
-print("-----------------------------")
-print(f'Total votes: {rowcount}')
-
-#Loops through candidates, printing results and calculating winner
-for name in list_of_candidates:
-    count = int(dict.get(name)) 
+candidates = []
+winner_count = 0
+# Loops through candidates, printing results and calculating winner
+for name, count in votes.items():
     percentage = count / rowcount * 100
-    print(f'{name}: {percentage:.3f}% ({count})')
+    candidates.append(f'{name}: {percentage:.3f}% ({count})')
     if count > winner_count:
         winner = name
         winner_count = count
 
-#Prints winner
-print("-----------------------------")
-print(f'Winner: {winner}')
-print("-----------------------------")
+results = [
+    'Election Results',
+    '-----------------------------',
+    f'Total votes: {rowcount}',
+    *candidates,
+    '-----------------------------',
+    f'Winner: {winner}',
+    '-----------------------------',
+]
 
-#Creates analysis file with same results
+print('\n'.join(results))
+
+# Creates analysis file with same results
 with open('analysis/analysis.txt', 'w') as f:
-    f.write(f'Election Results\n')
-    f.write(f'-----------------------------\n')
-    f.write(f'Total votes: {rowcount}\n')
-
-    #Loops through candidates, printing results and calculating winner
-    for name in list_of_candidates:
-        count = int(dict.get(name)) 
-        percentage = count / rowcount * 100
-        f.write(f'{name}: {percentage:.3f}% ({count})\n')
-        if count > winner_count:
-            winner = name
-            winner_count = count
-
-    #Prints winner
-    f.write(f'-----------------------------\n')
-    f.write(f'Winner: {winner}\n')
-    f.write(f'-----------------------------\n')
+    f.write('\n'.join(results))
